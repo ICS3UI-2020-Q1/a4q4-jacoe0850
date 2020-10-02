54416e6bedc0e677c9bb37cbb4fcf526dee8d134
jacoe0850 <jacoe0850@wrdsb.ca>
2020-10-02 23:51:00 +0000

    Finished fizzbuzz2

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
  *
- * @author 
+ * @jacoe0850 
  */
 public class Main {
 
@@ -9,7 +10,25 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    Scanner input = new Scanner(System.in);
+    //ask or integer
+System.out.println("Please enter a positive integer to see the FizzBuzz numbers");
+int num = input.nextInt();
+// decalre and initialize count
+int count = 0;
+while(count < num){
+  //increase value on count by 1
+  count = count + 1;
+  //check for fizz buzz answers
+  if ((count % 5 == 0) && (count % 3 == 0)){System.out.println("You should say FizzBuzz");
+  } else if ((count % 3 == 0)){
+    System.out.println("You should say Buzz");
+  }else if ((count % 5 == 0)){
+    System.out.println("You should say Buzz");
+  }else if ((count % 3 !=0) && (count % 5 !=0)){
+    System.out.println("You should say" + " " + count);
+  }
+}
     
   }
 }
